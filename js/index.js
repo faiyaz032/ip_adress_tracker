ca9dcc4ecd1283c4062b65f0385e0ff54575022e
Faiyaz Rahman <faiyazrahman03@gmail.com>
2021-04-18 16:18:10 +0600

    added displayMap

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
 'use strict';
+
+//!selectors
+const mapContainer = document.querySelector('#map');
+const loader = document.querySelector('.loader');
+
+let map;
+
 const getIpAdress = async function (userInput = '') {
    try {
       let req;
@@ -10,8 +17,23 @@ const getIpAdress = async function (userInput = '') {
       }
       if (!req.ok) throw new Error('There is a problem with the request');
       const res = await req.json();
+      const { lat, lng } = res.location;
+      displayMap(lat, lng);
    } catch (error) {
       console.error(error);
    }
+};
+
+const displayMap = function (lat, lng) {
+   loader.style.display = 'none';
+   mapContainer.style.display = 'block';
+
+   map = L.map('map').setView([lng, lng], 18);
+
+   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+   }).addTo(map);
+
+   L.marker([lat, lng]).addTo(map).bindPopup().openPopup();
 };
 getIpAdress();
